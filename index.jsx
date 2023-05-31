0a0eab95e58c254b7dc50f7fb1414d08f72b6a63
Alex DuBois <adubois@alumni.uci.edu>
2023-05-30 18:03:22 -0600

    fixed minor typo in App

--- index.jsx
+++ index.jsx
@@ -12,7 +12,7 @@ function App() {
         <Link className="site-logo" to="/">#VanLife</Link>
         <nav>
           <Link to="/about">About</Link>
-          <Link to="/vans?">Vans</Link>
+          <Link to="/vans">Vans</Link>
         </nav>
       </header>
       <Routes>
