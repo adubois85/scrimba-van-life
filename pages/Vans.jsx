4994b4f9189c9b25c39b990918a70f4a6de1c20f
Alex DuBois <adubois@alumni.uci.edu>
2023-05-30 19:05:24 -0600

    almost working Van component

--- pages/Vans.jsx
+++ pages/Vans.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 export default function Vans() {
+    const [vans, setVans] = useState([])
+    useEffect(() => {
+        fetch("/api/vans")
+        .then(res => res.json())
+        .then(data => setVans(data.vans) )
+    }, [])
+
+    const vansElements = vans.map(van => {
+        <div key={van.id} className="van-tile">
+            <img src={van.imageUrl} />
+            <div className="van-info">
+                <h3>{van.name}</h3>
+                <p>${van.price}<span>/day</span></p>
+            </div>
+            <i className={`van-type ${van.type} selected`}>{van.type}</i>
+        </div>
+    })
+
     return (
-        <div>Vans</div>
+        <>
+            {vansElements}
+        </>
     )
 }
