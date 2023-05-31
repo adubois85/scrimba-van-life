6112cc4a3dca76a0289493789317659591d24bf0
Alex DuBois <adubois@alumni.uci.edu>
2023-05-30 19:18:30 -0600

    copied styling from design files

--- pages/Vans.jsx
+++ pages/Vans.jsx
@@ -20,8 +20,10 @@ export default function Vans() {
     ))
 
     return (
-        <>
-            {vanElements}
-        </>
+        <div className="van-list-container">
+            <div className="van-list">
+                {vanElements}
+            </div>
+        </div>
     )
 }
