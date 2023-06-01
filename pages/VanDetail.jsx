22893087740ab052dc702d2dd294b6934336b8f1
Alex DuBois <adubois@alumni.uci.edu>
2023-05-31 18:07:56 -0600

    using params in vanDetail component

--- pages/VanDetail.jsx
+++ pages/VanDetail.jsx
@@ -1,5 +1,12 @@
-import React from "react"
+import React, { useEffect } from "react"
+import { useParams } from 'react-router-dom'
 
 export default function VanDetail() {
+    const params = useParams()
+    const van = useEffect(() => {
+        fetch(`/api/vans/${params.id}`)
+            .then(res => res.json())
+            .then(data => console.log(data))
+    }, [params.id])
     return <h1>Van detail page goes here</h1>
 }
