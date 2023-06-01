40f7523382648ad230fa131c7969e6e308d071ce
Alex DuBois <adubois@alumni.uci.edu>
2023-05-31 18:27:44 -0600

    copied stying over from design docs

--- pages/VanDetail.jsx
+++ pages/VanDetail.jsx
@@ -1,12 +1,27 @@
-import React, { useEffect } from "react"
+import React, { useState, useEffect } from "react"
 import { useParams } from 'react-router-dom'
 
 export default function VanDetail() {
+    const [van, setVan] = useState()
     const params = useParams()
-    const van = useEffect(() => {
+    useEffect(() => {
         fetch(`/api/vans/${params.id}`)
             .then(res => res.json())
-            .then(data => console.log(data))
+            .then(data => setVan(data.vans))
     }, [params.id])
-    return <h1>Van detail page goes here</h1>
+    
+    return (
+        <div className="van-detail-container">
+            {van ? (
+                <div className="van-detail">
+                    <img src={van.imageUrl} />
+                    <i className={`van-type ${van.type} selected`}>{van.type}</i>
+                    <h2>{van.name}</h2>
+                    <p className="van-price"><span>${van.price}</span>/day</p>
+                    <p>{van.description}</p>
+                    <button className="link-button">Rent this van</button>
+                </div>
+            ) : <h2>Loading...</h2>}
+        </div>
+    )
 }
