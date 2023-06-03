12b246772675e10ae306b7b87906d55c3f77cc37
Alex DuBois <adubois@alumni.uci.edu>
2023-06-02 18:35:23 -0600

    start of moving Header to its own component

--- index.jsx
+++ index.jsx
@@ -1,24 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from "./pages/Home"
 import About from "./pages/About"
 import Vans from './pages/Vans'
 import VanDetail from "./pages/VanDetail"
-
 
 import "./server"
 
 function App() {
   return (
     <BrowserRouter>
-      <header>
-        <Link className="site-logo" to="/">#VanLife</Link>
-        <nav>
-          <Link to="/about">About</Link>
-          <Link to="/vans">Vans</Link>
-        </nav>
-      </header>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
