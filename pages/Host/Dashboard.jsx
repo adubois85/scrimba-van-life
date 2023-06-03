d5e8c63cac1755edf555406feb67cc7498e9c2db
Alex DuBois <adubois@alumni.uci.edu>
2023-06-03 17:56:11 -0600

    started turning Dashboard into a Layout route

--- pages/Host/Dashboard.jsx
+++ pages/Host/Dashboard.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
 
 export default function Dashboard() {
     return (
-        <div>Dashboard</div>
+        <>
+            <div>Dashboard</div>
+            <Outlet />
+        </>
+
     )
 }
