e0a375235d3eab4bb3841b3ea0b814d6d9bb49bd
Alex DuBois <adubois@alumni.uci.edu>
2023-06-04 17:40:30 -0600

    beginnings of a working HostVans component

--- pages/Host/HostVans.jsx
+++ pages/Host/HostVans.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 export default function HostVans() {
+    const [vans, setVans] = useState([])
+    useEffect(() => {
+        fetch("/api/host/vans")
+        .then(res => res.json())
+        .then(data => setVans(data.vans))
+    }, [])
+
+    const vansListings = vans.map(van => (
+        <div key={van.id} className='host-van-tile'>
+            <img src={van.imageUrl} />
+            <h3>{van.name}</h3>
+            <p>{van.price}</p>
+        </div>
+    ))
+
     return (
-        <h1>Vans</h1>
+        <>
+            {vansListings}
+        </>
     )
 }
