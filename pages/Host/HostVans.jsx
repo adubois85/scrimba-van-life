bfd88af7034c122a24b81fad6fed06848eff0e05
Alex DuBois <adubois@alumni.uci.edu>
2023-06-05 19:14:16 -0600

    copied HostVans file so styling would take effect

--- pages/Host/HostVans.jsx
+++ pages/Host/HostVans.jsx
@@ -1,26 +1,46 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState, useEffect } from "react"
+import { Link } from "react-router-dom"
 
 export default function HostVans() {
     const [vans, setVans] = useState([])
+
     useEffect(() => {
         fetch("/api/host/vans")
-        .then(res => res.json())
-        .then(data => setVans(data.vans))
+            .then(res => res.json())
+            .then(data => setVans(data.vans))
     }, [])
 
-    const vansListings = vans.map(van => (
-        <div key={van.id} className='host-van-tile'>
-            <img src={van.imageUrl} />
-            <div className='van-host-tile-details'>
-                <h3>{van.name}</h3>
-                <p>${van.price}/day</p>
+    const hostVansEls = vans.map(van => (
+        <Link
+            to={`/host/vans/${van.id}`}
+            key={van.id}
+            className="host-van-link-wrapper"
+        >
+            <div className="host-van-single" key={van.id}>
+                <img src={van.imageUrl} alt={`Photo of ${van.name}`} />
+                <div className="host-van-info">
+                    <h3>{van.name}</h3>
+                    <p>${van.price}/day</p>
+                </div>
             </div>
-        </div>
+        </Link>
     ))
 
     return (
-        <>
-            {vansListings}
-        </>
+        <section>
+            <h1 className="host-vans-title">Your listed vans</h1>
+            <div className="host-vans-list">
+                {
+                    vans.length > 0 ? (
+                        <section>
+                            {hostVansEls}
+                        </section>
+
+                    ) : (
+                            <h2>Loading...</h2>
+                        )
+                }
+            </div>
+        </section>
     )
 }
