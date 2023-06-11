1f6e68e5d82f3745f0f74ba92db091a9011646ad
Alex DuBois <adubois@alumni.uci.edu>
2023-06-10 19:02:17 -0600

    conditionally render styling for selected van type

--- pages/Vans/Vans.jsx
+++ pages/Vans/Vans.jsx
@@ -33,11 +33,28 @@ export default function Vans() {
         <div className="van-list-container">
             <h1>Explore our van options</h1>
             <div className="van-list-filter-buttons">
-                <button onClick={() => setSearchParams({type: "simple"})} className='van-type simple'>Simple</button>
-                <button onClick={() => setSearchParams({type: "rugged"})} className='van-type rugged'>Rugged</button>
-                <button onClick={() => setSearchParams({type: "luxury"})} className='van-type luxury'>Luxury</button>
+                <button 
+                    onClick={() => setSearchParams({type: "simple"})}
+                    className={`van-type simple ${typeFilter === "simple" ? "selected" : ""}`}
+                    >Simple
+                </button>
+                <button
+                    onClick={() => setSearchParams({type: "rugged"})}
+                    className={`van-type rugged ${typeFilter === "rugged" ? "selected" : ""}`}
+                    >Rugged
+                </button>
+                <button
+                    onClick={() => setSearchParams({type: "luxury"})}
+                    className={`van-type luxury ${typeFilter === "luxury" ? "selected" : ""}`}
+                    >Luxury
+                </button>
                 {typeFilter && 
-                <button onClick={() => setSearchParams({})} className='van-type clear-filters'>Clear Filters</button>}
+                <button
+                    onClick={() => setSearchParams({})}
+                    className='van-type clear-filters'
+                    >Clear Filters
+                </button>
+                }
             </div>
             <div className="van-list">
                 {vanElements}
