3569268fd01c1b1ae0e7ac0382e5112ba8d2daf1
Alex DuBois <adubois@alumni.uci.edu>
2023-06-10 18:55:08 -0600

    conditionally render the 'Clear Filters' button

--- pages/Vans/Vans.jsx
+++ pages/Vans/Vans.jsx
@@ -36,7 +36,8 @@ export default function Vans() {
                 <button onClick={() => setSearchParams({type: "simple"})} className='van-type simple'>Simple</button>
                 <button onClick={() => setSearchParams({type: "rugged"})} className='van-type rugged'>Rugged</button>
                 <button onClick={() => setSearchParams({type: "luxury"})} className='van-type luxury'>Luxury</button>
-                <button onClick={() => setSearchParams({})} className='van-type clear-filters'>Clear Filters</button>
+                {typeFilter && 
+                <button onClick={() => setSearchParams({})} className='van-type clear-filters'>Clear Filters</button>}
             </div>
             <div className="van-list">
                 {vanElements}
