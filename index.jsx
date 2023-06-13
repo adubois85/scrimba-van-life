77dcba876e8ad4396d097612e0fcd9fb7d0a8af2
Alex DuBois <adubois@alumni.uci.edu>
2023-06-13 16:12:31 -0600

    little bit of code cleanup

--- index.jsx
+++ index.jsx
@@ -5,24 +5,25 @@ import {
     createBrowserRouter,
     createRoutesFromElements,
     Route 
-} from "react-router-dom"
-import Home from "./pages/Home"
-import About from "./pages/About"
-import Vans, { loader as vansLoader } from './pages/Vans/Vans'
-import VanDetail, { loader as VanDetailLoader} from "./pages/Vans/VanDetail"
+} from 'react-router-dom'
 import Layout from './components/Layout';
+import Home from './pages/Home'
+import About from './pages/About'
+import Login from './pages/Login'
+import NotFound from './pages/NotFound'
 import HostLayout from './components/HostLayout'
-import Dashboard from './pages/Host/Dashboard';
-import Income from './pages/Host/Income';
-import Reviews from './pages/Host/Reviews';
+import Error from './components/Error';
+import Vans, { loader as vansLoader } from './pages/Vans/Vans'
+import VanDetail, { loader as VanDetailLoader} from './pages/Vans/VanDetail'
+import Dashboard from './pages/Host/Dashboard'
+import Income from './pages/Host/Income'
+import Reviews from './pages/Host/Reviews'
 import HostVans, { loader as hostVansLoader } from './pages/Host/HostVans'
 import HostVanDetail, { loader as hostVanDetailLoader } from './pages/Host/HostVanDetail'
 import HostVanInfo from './pages/Host/HostVanInfo'
 import HostVanPhotos from './pages/Host/HostVanPhotos'
 import HostVanPricing from './pages/Host/HostVanPricing'
-import NotFound from './pages/NotFound'
-import Error from './components/Error';
-import Login from './pages/Login'
+import { requireAuth } from './utils'
 
 import "./server"
 
@@ -30,7 +31,7 @@ const router = createBrowserRouter(createRoutesFromElements(
         <Route path="/" element={<Layout />}>
             <Route index element={<Home />} />
             <Route path="about" element={<About />} />
-            <Route  path="login" element={<Login />} />
+            <Route path="login" element={<Login />} />
             <Route path="vans" element={<Vans />} loader={vansLoader} errorElement={<Error />}/>
             <Route path="vans/:id" element={<VanDetail />} loader={VanDetailLoader} />
             <Route path='host' element={<HostLayout />}>
