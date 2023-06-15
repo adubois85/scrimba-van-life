a01b016fc61b512bde73ee261b91e506d7885e7c
Alex DuBois <adubois@alumni.uci.edu>
2023-06-15 16:50:58 -0600

    disable button and show error on bad creds

--- pages/Login.jsx
+++ pages/Login.jsx
@@ -1,5 +1,5 @@
-import React from "react"
-import { useLoaderData } from "react-router-dom"
+import React, { useState } from 'react'
+import { useLoaderData } from 'react-router-dom'
 import { loginUser } from "../api"
 
 export function loader({ request }) {
@@ -7,13 +7,20 @@ export function loader({ request }) {
 }
 
 export default function Login() {
-    const [loginFormData, setLoginFormData] = React.useState({ email: "", password: "" })
+    const [loginFormData, setLoginFormData] = useState({ email: "", password: "" })
+    const [status, setStatus] = useState("idle")
+    const [error, setError] = useState(null)
+
     const message = useLoaderData()
 
     function handleSubmit(e) {
         e.preventDefault()
+        setStatus("submitting")
+        setError(null)
         loginUser(loginFormData)
         .then(data => console.log(data))
+        .catch(err => setError(err))
+        .finally(setStatus("idle"))
     }
 
     function handleChange(e) {
@@ -27,7 +34,8 @@ export default function Login() {
     return (
         <div className="login-container">
             <h1>Sign in to your account</h1>
-            {message && <h3 className="red">{message}</h3>}
+            {message && <h3 className='red'>{message}</h3>}
+            {error && <h3 className='red'>{error.message}</h3>}
             <form onSubmit={handleSubmit} className="login-form">
                 <input
                     name="email"
@@ -43,7 +51,7 @@ export default function Login() {
                     placeholder="Password"
                     value={loginFormData.password}
                 />
-                <button>Log in</button>
+                <button disabled={status === "submitting" ? true : false}>Log in</button>
             </form>
         </div>
     )
